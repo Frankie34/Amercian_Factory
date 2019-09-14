17aab822995abef98f3779b60b1c9caa6ce3412e
Juncheng Zhu <zhujuncheng1021@163.com>
2019-09-14 18:21:07 +0800

    init(Frontend)structure

---
+++ america-factory/src/components/HomePage.vue
@@ -0,0 +1,100 @@
+<template>
+    <el-container>
+    <el-aside width="200px">
+       <el-col>
+    <h5>Default colors</h5>
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>Navigator One</span>
+        </template>
+        <el-menu-item-group title="Group One">
+          <el-menu-item index="1-1">item one</el-menu-item>
+          <el-menu-item index="1-2">item one</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="Group One">
+          <el-menu-item index="1-1">item one</el-menu-item>
+          <el-menu-item index="1-2">item one</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="Group One">
+          <el-menu-item index="1-1">item one</el-menu-item>
+          <el-menu-item index="1-2">item one</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
+  </el-col>
+    </el-aside>  
+        <el-main>
+            <el-header>Dashboard</el-header>
+            {{msg}}
+        </el-main>
+    </el-container>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data() {
+      return {
+          msg: 'hello',
+      }
+  },
+  methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+  },
+}
+</script>
+
+<style>
+.el-header, .el-footer {
+background-color: #474a57;
+color: #B3C0D1;
+text-align: left;
+line-height: 60px;
+}
+
+.el-aside {
+background-color: #343640;
+color: #D3DCE6;
+text-align: center;
+line-height: 50px;
+}
+
+.el-main {
+background-color: #474a57;
+color: #D3DCE6;
+text-align: center;
+line-height: 700px;
+}
+
+body > .el-container {
+margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+line-height: 320px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
